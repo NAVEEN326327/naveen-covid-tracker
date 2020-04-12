856f447d88282326a4939963d7610ac78aca5a95
NAVEEN326327 <nks326327@gmail.com>
2020-04-12 23:13:20 +0530

    Bar Chart and country picker completed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { fetchData } from './api';
 class App extends React.Component {
     state = {
         data: {},
+        country: '',
     }
 
 
@@ -15,12 +16,20 @@ class App extends React.Component {
         this.setState( { data: fetchedData } );
     }
 
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+      this.setState({ data: fetchedData, country: country})
+
+// fetch the data
+// set the state
+    }
     render() {
+        const {data, country } = this.state;
         return (
             <div className={styles.container}>
                 <Cards data= {this.state.data} /> 
-                <CountryPicker />
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
             </div>
         );
     }f
